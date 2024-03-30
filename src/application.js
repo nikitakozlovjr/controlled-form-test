8e9d3e665907a671012648711db968ebf1a798de
nikitakozlovjr <nikita3726898@gmail.com>
2024-03-30 21:36:49 +0300

    [STEP 2 - feat] add post request after submit button

--- src/application.js
+++ src/application.js
@@ -1,5 +1,45 @@
-import render from './index.js';
+import onChange from 'on-change';
+import axios from 'axios';
+import renderForm from './index.js';
+
+const render = (state) => (path, value) => {
+  if (path === 'process') {
+    if (value === 'processed') {
+      const body = document.querySelector('body');
+      body.innerHTML = `<p>${state.message}</p>`;
+    }
+  }
+};
 
 export default () => {
-  render();
+  const state = {
+    name: {
+      value: null,
+      error: null,
+    },
+    email: {
+      value: null,
+      error: null,
+    },
+    process: 'filling',
+    message: null,
+  };
+
+  renderForm();
+
+  const watchedState = onChange(state, render(state));
+
+  const btn = document.querySelector('.btn');
+
+  btn.addEventListener('click', async () => {
+    try {
+      const { data } = await axios.post('/users');
+      state.message = data.message;
+      watchedState.process = 'processed';
+    } catch {
+      throw new Error(
+        'Problems with sending data to the server. Check your internet connection',
+      );
+    }
+  });
 };
